97c3d15f74b4797b2d86a7c5485696568da50f23
Jessica Poe <jesspoe00@gmail.com>
2019-04-12 17:17:40 -0700

    Created Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BooksContainer from './Containers/BooksContainer'
+import NavContainer from './Containers/NavContainer'
+
 const BOOKAPI = `http://localhost:3000/books`
 class App extends Component {
-   constructor (){
+   constructor() {
       super()
       this.state = {
          books: []
       }
+   };
+
+   componentDidMount() {
+      this.grabBooks()
+         .then(books => console.log(this.state.books))
    }
 
-   componentDidMount(){
-        fetch(BOOKAPI)
-          .then(response => response.json())
-          .then(json => {
-             this.setState({
-                books: json
-             })
-             console.log(json[0].paragraph)
+   grabBooks = () => {
+      return fetch(BOOKAPI)
+         .then(response => response.json())
+         .then(json => {
+            this.setState({
+               books: json
+            })
          })
    }
 
-  render() {
-    return (
-      <div>
-         <BooksContainer books={this.state.books}/>
-      </div>
-
-    );
-  }
+   render() {
+      return (
+         <NavContainer books={this.state.books} />
+      )
+   }
 }
 
+
 export default App;
+
--- src/Containers/BooksContainer.js
+++ src/Containers/BooksContainer.js
@@ -6,9 +6,9 @@ class BooksContainer extends Component {
    render() {
       return (
          <div>
-               {this.props.books.map((book)=> {
-                  return <Book book={book}/>
-               })}
+            {this.props.books.map((book) => {
+               return <Book book={book} />
+            })}
          </div>
       );
    }
