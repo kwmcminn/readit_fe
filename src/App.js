96268446141e378a9d351f46b6877880266a5e11
Jessica Poe <jesspoe00@gmail.com>
2019-04-15 12:01:49 -0700

    added react ui semantics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import MenuExampleEvenlyDivided from './Containers/Menu'
 import BooksContainer from './Containers/BooksContainer'
 const BOOKAPI = `http://localhost:3000/books`
 class App extends Component {
@@ -10,7 +11,13 @@ class App extends Component {
          kindergarten: [],
          first: [],
          second: [],
-         third: []
+         third: [],
+         bookShowing: false,
+         myBooksIndex: 0,
+         kindergartenIndex: 0,
+         firstIndex: 0,
+         secondIndex: 0,
+         thirdIndex: 0
       }
       this.grabBooks = this.grabBooks.bind(this)
       this.fetchingGrades = this.fetchingGrades.bind(this)
@@ -43,15 +50,47 @@ class App extends Component {
       })
    }
 
-   render() {
-      return (
-         <div className='app-container' >
-            <BooksContainer books={this.state.books} kindergarten={this.state.kindergarten} first={this.state.first} second={this.state.second} third={this.state.third} />
-         </div>
-      )
+   showBookDetails = () => {
+      this.setState({
+         bookShowing: true
+      })
    }
 
 
+   nextBooks = () => {
+      this.setState({
+         index: this.state.index + 1
+      })
+   }
+
+   lastBooks = () => {
+      this.setState({
+         index: this.state.index - 1
+      })
+   }
+
+   render() {
+      return (
+         <div className='app-container' >
+            <MenuExampleEvenlyDivided />
+            <div className='books-container'>
+               <div className='grade-container'></div>
+               <div className='grade-title'></div>
+               <div className='single-book-container'>
+                  <div className='single-book'>
+                     <h4>Title</h4>
+                     <img src="https://carwad.net/sites/default/files/cute-balloon-cliparts-125087-3969158.jpg" />
+                     <h5>Author</h5>
+                  </div>
+               </div>
+               <div className='grade-container'></div>
+               <div className='grade-container'></div>
+               <div className='grade-container'></div>
+               <div className='grade-container'></div>
+            </div>
+         </div>
+      )
+   }
 }
 
-export default App;
+export default App
--- src/Containers/BooksContainer.js
+++ src/Containers/BooksContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import GradeContainer from './GradeContainer';
 import Book from '../Component/Book'
 
 class BooksContainer extends Component {
@@ -7,33 +6,43 @@ class BooksContainer extends Component {
 
    render() {
       return (
-          <div className='book-container'>
-             <div className='my-books'>
-             </div>
-               <div className='kindergarten'>
-                  {this.props.kindergarten.map((book) => {
-                     return <Book book={book}/>
-                  })}
-               </div>
-               <div className='first-grade'>
-                  {this.props.first.map((book) => {
-                     return <Book book={book}/>
-                  })}
-               </div>
-               <div className='second-grade'>
-                  {this.props.second.map((book) => {
-                     return <Book book={book}/>
-                  })}
-               </div>
-               <div className='third-grade'>
-                  {this.props.third.map((book) => {
-                     return <Book book={book}/>
-                  })}
-               </div>
+         <div className='book-container'>
+            <div className='kindergarten'>
+               <h1 className='grade-header'>Kindergarten</h1>
+               {this.props.kindergarten.slice(this.props.kindergartenIndex, this.props.kindergartenIndex + 3).map((book) => {
+                  return <Book book={book} showBookDetails={this.props.showBookDetails} />
+               })}
+            </div>
          </div>
       );
    }
 }
+// <div className='my-books'>
+// </div >
+// <div className='kindergarten'>
+//    <h1 className='grade-header'>Kindergarten</h1>
+//    {this.props.kindergarten.slice(this.props.kindergartenIndex, this.props.kindergartenIndex + 3).map((book) => {
+//       return <Book book={book} showBookDetails={this.props.showBookDetails}/>
+//    })}
+// </div>
+// <div className='first-grade'>
+//    <h1 className='grade-header'>First Grade</h1>
+//    {this.props.first.slice(this.props.firstIndex, this.props.firstIndex + 3).map((book) => {
+//       return <Book book={book} showBookDetails={this.props.showBookDetails}/>
+//    })}
+// </div>
+// <div className='second-grade'>
+//    <h1 className='grade-header'>Second Grade</h1>
+//    {this.props.second.slice(this.props.secondIndex, this.props.secondIndex + 3).map((book) => {
+//       return <Book book={book} showBookDetails={this.props.showBookDetails}/>
+//    })}
+// </div>
+// <div className='third-grade'>
+//    <h1 className='grade-header'>Third Grade</h1>
+//    {this.props.third.slice(this.props.thirdIndex, this.props.thirdIndex + 3).map((book) => {
+//       return <Book book={book} showBookDetails={this.props.showBookDetails}/>
+//    })}
+// </div>
 
 // <GradeContainer kindergarten={this.props.kindergarten} className='kindergarten' />
 // <GradeContainer first={this.props.first} className='first-grade' />
